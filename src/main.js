2535ef475ca01cb886c06d9c97f6ac92b7219c95
dirstart <q835041058@gmail.com>
2020-08-05 22:50:43 +0800

    test-play:iview

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
+import iView from 'iview'
+
+
 import App from './App.vue'
+import router from './router'
+import 'iview/dist/styles/iview.css'
 
 Vue.config.productionTip = false
 
+Vue.use(iView)
+
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    123
+    <Page :current="1" :total="100"></Page>
+    <Steps :current="1">
+        <Step title="已完成" content="这里是该步骤的描述信息"></Step>
+        <Step title="进行中" content="这里是该步骤的描述信息"></Step>
+        <Step title="待进行" content="这里是该步骤的描述信息"></Step>
+        <Step title="待进行" content="这里是该步骤的描述信息"></Step>
+    </Steps>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  created() {},
+  mounted() {},
+  methods: {}
+};
+</script>
+
+<style>
+</style>
